dbbafee094195f75a022c7cfd25ed7e245b15e16
Tom Beynon <tbeynon@rawnet.com>
2013-05-01 12:58:00 +0100

    Moved rails_app to external cookbook

--- site-cookbooks/rails_app/recipes/default.rb
+++
@@ -1,8 +0,0 @@
-#
-# Author:: Tom Beynon (tbeynon@rawnet.com) // Dan Upton (dupton@rawnet.com)
-# Cookbook Name:: rails_app
-# Recipe:: default
-#
-
-include_recipe "rails_app::files"
-include_recipe "rails_app::databases"
