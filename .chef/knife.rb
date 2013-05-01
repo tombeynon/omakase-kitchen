175d1c11a7f5d790316c0acce711978cc644d894
Tom Beynon <tbeynon@rawnet.com>
2013-05-01 12:19:27 +0100

    Moved knife config to .chef and set cookbook_path

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,2 +1,5 @@
 # Workaround for: https://github.com/matschaffer/knife-solo/issues/212
 knife[:solo_path] = "~/chef-solo"
+
+current_dir = File.dirname(__FILE__)
+cookbook_path ["#{current_dir}/../cookbooks"]
--- Vagrantfile
+++ Vagrantfile
@@ -15,6 +15,5 @@ Vagrant.configure("2") do |config|
 		db_config.vm.network :private_network, 
 			ip: "192.168.2.11"
 	end
-	
   
 end
